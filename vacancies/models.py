2e1acd161df416eb6806855230e95b05deda5bd8
tote10 <motialemu9@gmail.com>
2025-11-08 14:42:44 +0300

    Stop tracking files ignored by .gitignore

---
+++ vacancies/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+class job(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    posted_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+class Application(models.Model):
+    job = models.ForeignKey(job, on_delete=models.CASCADE)
+    applicant_name = models.CharField(max_length=100)
+    applicant_email = models.EmailField()
+    resume = models.FileField(upload_to='resumes/')
+    cover_letter = models.TextField()
+    applied_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.applicant_name} - {self.job.title}"
