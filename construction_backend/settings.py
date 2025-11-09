d616749a559b9c7d50fc52eb3cf2494505b62b3d
tote10 <motialemu9@gmail.com>
2025-11-09 10:15:14 +0300

    Add CORS headers middleware and fix typo in urlpatterns variable name

--- construction_backend/settings.py
+++ construction_backend/settings.py
@@ -41,10 +41,13 @@ INSTALLED_APPS = [
     'projects',
     'rest_framework',
     'services',
+    'corsheaders',
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
