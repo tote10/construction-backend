83bfaba255a9db35c5ef3bce754c2ada04376c27
tote10 <motialemu9@gmail.com>
2025-12-12 00:09:59 +0300

    Refactor code structure for improved readability and maintainability

--- construction_backend/urls.py
+++ construction_backend/urls.py
@@ -4,6 +4,8 @@ from rest_framework import routers
 from projects.views import ProjectUpdateViewSet, ProjectViewSet
 from services.views import ServiceViewSet
 from vacancies.views import JobViewSet, ApplicationViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet)
@@ -15,3 +17,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
