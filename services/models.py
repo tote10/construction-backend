d06d7d7eb9efcf2c36dddedb91938e255f1f0081
tote10 <motialemu9@gmail.com>
2025-12-19 13:46:56 +0300

    feat: Add Service model with title, description, and a default icon field.

--- services/models.py
+++ services/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Service(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    icon = models.ImageField(upload_to='services/', blank=True, null=True)
+    icon = models.CharField(max_length=100, default='Building2')
 
     def __str__(self):
         return self.title
